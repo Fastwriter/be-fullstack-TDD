76c0008319c2e56481458c0c85497b62f0addf89
Fastwriter <demeuov.daulet.nis.ura@gmail.com>
2018-10-12 01:06:56 +0600

    Test: #4 find_max.py

--- find_max.py
+++ find_max.py
@@ -12,12 +12,13 @@ def get_max_with_one_argument(a):
 def get_max_with_many_arguments(*args):
     maximum = float('inf')
     for a in args:
-    	if a < maximum:
+    	if a > maximum:
     		maximum = a
     return maximum
 
 def get_max_with_one_or_more_arguments(first, *args):
-    pass
+    maximum = float('inf')
+    for a 
 
 
 def get_max_bounded(*args, low, high):
