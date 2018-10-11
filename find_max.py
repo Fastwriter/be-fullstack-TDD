581ba767356c04fb1504bf376208de3624e65380
Fastwriter <demeuov.daulet.nis.ura@gmail.com>
2018-10-12 01:18:50 +0600

    Test: find_max.py

--- find_max.py
+++ find_max.py
@@ -33,7 +33,6 @@ def get_max_bounded(*args, low, high):
 def make_max(*, low, high):
     def inner(first, *args):
         maximum = 0
-
         for a in (first,)+args:
     		if a > maximum and low<a<high:
     			maximum = a
