dc7be8f9fdc9698cfb617ecb6ab3edb2a3964356
sansyzbaev <sansyzbaev.islam@gmail.com>
2018-10-12 00:22:27 +0600

    Test: re-write find_min.py

--- find_min.py
+++ find_min.py
@@ -4,37 +4,49 @@ def get_min(a, b):
     """
         return min number among a and b
     """
-
+    return a if a < b else b
 
 def get_min_without_arguments():
     """
         raise TypeError exception with message
     """
-
+    raise TypeError('arguments are miseed')
 
 def get_min_with_one_argument(x):
     """
         return that value
     """
-
+    return x
 
 def get_min_with_many_arguments(*args):
     """
         return smallest number among args
     """
-
+    minimum = float('inf')
+    for a in args:
+        if a < minimum:
+            minimum = a
+    return minimum
 
 def get_min_with_one_or_more_arguments(first, *args):
     """
         return smallest number among first + args
     """
-
+    minimum = float('inf')
+    for a in (first,)+args:
+        if a < minimum:
+            minimum = a
+    return minimum
 
 def get_min_bounded(*args, low, high):
     """
         return smallest number among args bounded by low & high
     """
-
+    minimum = float('inf')
+    for a in args:
+        if a < minimum and low<a<high:
+            minimum = a
+    return minimum
 
 def make_min(*, low, high):
     """
@@ -45,6 +57,11 @@ def make_min(*, low, high):
     """
 
     def inner(first, *args):
-        pass
+        minimum = float('inf')
+
+        for a in (first,)+args:
+            if a<minimum and low<a<high:
+                minimum = a
+        return minimum
 
     return inner
